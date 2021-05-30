c3c9f1bd31b102bace8348aa4f6182786372e56b
ckstlr0828 <77110203+ckstlr0828@users.noreply.github.com>
2021-05-31 03:51:35 +0900

    수정중
    수정중

--- 웹포트폴리오/html/lirikosmarine/js/productDetailsEvent.js
+++ 웹포트폴리오/html/lirikosmarine/js/productDetailsEvent.js
@@ -8,28 +8,28 @@ $(document).ready(function(){
         
         if(mql_1.matches){
              if(productScroll > $("#product_sub_info").offset().top){
-                $("#product_title_wrap").css({"position":"absolute","bottom":"6rem"});
+                $("#product_title_wrap").css({"position":"absolute","top":"auto", "bottom":"9.5rem"});
             }else{
-                $("#product_title_wrap").css({"position":"fixed", "bottom":"6.35rem"});
+                $("#product_title_wrap").css({"position":"fixed", "top":"6rem","bottom":"auto"});
             }
         }else if(mql_2.matches){
             if(productScroll > $("#product_sub_info").offset().top){
-                $("#product_title_wrap").css({"position":"absolute","bottom":"6rem"});
+                $("#product_title_wrap").css({"position":"absolute","top":"auto", "bottom":"6rem"});
             }else{
-                $("#product_title_wrap").css({"position":"fixed", "bottom":"7.35rem"});
+                $("#product_title_wrap").css({"position":"fixed", "top":"6rem", "bottom":"7.35rem"});
             }
 
         }else if(mql_3.matches){
             if(productScroll > $("#product_sub_info").offset().top){
-                $("#product_title_wrap").css({"position":"relative","bottom":"-1.65rem"});
+                $("#product_title_wrap").css({"position":"relative","top":"auto", "bottom":"-1.65rem"});
             }else{
-                $("#product_title_wrap").css({"position":"relative", "bottom":"-1.65rem"});
+                $("#product_title_wrap").css({"position":"relative", "top":"2rem", "bottom":"-1.65rem"});
             }
         }else{
             if(productScroll > $("#product_sub_info").offset().top){
-                $("#product_title_wrap").css({"position":"relative","bottom":"-1.65rem"});
+                $("#product_title_wrap").css({"position":"relative","top":"auto", "bottom":"-1.65rem"});
             }else{
-                $("#product_title_wrap").css({"position":"relative", "bottom":"-1.65rem"});
+                $("#product_title_wrap").css({"position":"relative", "top":"2rem", "bottom":"-1.65rem"});
             }
         }
     });
