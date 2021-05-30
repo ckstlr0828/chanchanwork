1a805a90b8290f82923fe08bcc5587925275c503
ckstlr0828 <77110203+ckstlr0828@users.noreply.github.com>
2021-05-31 04:00:32 +0900

    ㄴㄴ
    ㄴㄴㄴ

--- 웹포트폴리오/html/lirikosmarine/js/productDetailsEvent.js
+++ 웹포트폴리오/html/lirikosmarine/js/productDetailsEvent.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
     var mql_3 = window.matchMedia("screen and (min-width:480px) and (max-width:767px)");
 
     $(window).scroll(function(){
-        var productScroll = $(window).scrollTop() + $(window).height();
+        var productScroll = $(window).scrollTop() + $(window).height()/1.1;
         
         if(mql_1.matches){
              if(productScroll > $("#product_sub_info").offset().top){
-                $("#product_title_wrap").css({"position":"absolute","top":"auto", "bottom":"9.5rem"});
+                $("#product_title_wrap").css({"position":"absolute","top":"auto", "bottom":"6.5rem"});
             }else{
                 $("#product_title_wrap").css({"position":"fixed", "top":"6rem","bottom":"auto"});
             }
